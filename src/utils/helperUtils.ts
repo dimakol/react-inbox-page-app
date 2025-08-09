32623ffb87fbefca679a3ef5ac72054fdd2da8eb
Dmitry Kolyas <dimakol@gmail.com>
2025-08-09 14:09:59 +0300

    refactor(contact): remove PhoneType declaration
    - Use `string` type for phone numbers in Contact type.
    - Update utility functions to reflect the change.

--- src/utils/helperUtils.ts
+++ src/utils/helperUtils.ts
@@ -1,16 +1,16 @@
 // Import types
-import type { Contact, PhoneType } from "../types/contact";
+import type { Contact } from "../types/contact";
 import type { Conversation } from "../types/conversation";
 
 export const getContactByPhone = (
-  phone: PhoneType,
+  phone: string,
   contactsData: Contact[]
 ): Contact | undefined => {
   return contactsData.find((contact) => contact.phone === phone);
 };
 
 export const getConversationByPhone = (
-  phone: PhoneType,
+  phone: string,
   conversationsData: Conversation[]
 ): Conversation | undefined => {
   return conversationsData.find((conversation) => conversation.phone === phone);
